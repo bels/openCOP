5c0acb6bd7203bfcf7cc8802f471c52d3e7e3658
bels <bels11@yahoo.com>
2010-08-31 10:52:41 -0400

    - Fixed the bugs in the postgresql file - Created a shell script that will assist the implementor in setting up the database

---
+++ sql/setup_postgresql.sh
@@ -0,0 +1,29 @@
+#!/usr/local/bin/bash
+
+DATABASE="ccboehd"
+
+echo "This script makes some assumptions.  Let me get them out of the way right now"
+echo "1. You are running this as a user that has superuser privileges in the database"
+echo "2. That you have createdb installed and available in that user's PATH"
+echo "3. That psql is available in that user's PATH"
+echo "4. You are running this script from the same directory as postgresql.sql"
+
+echo "With that said, lets begin"
+echo "Creating database"
+createdb $DATABASE
+if [ $? != 0 ]
+then
+{
+	echo "Couldn't create the database. Check my assumptions that I listed and then re-run this script."
+	exit 1
+} fi
+
+echo "Importing database schema\n"
+psql $DATABASE < postgresql.sql
+if [ $? != 0 ]
+then
+{
+	echo "Couldn't import the database. Check my assumptions that I listed and then re-run this script."
+	exit 1
+} fi
+echo "All Done. Enjoy!\n"
