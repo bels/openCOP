636817fe4a45b44a7589808fae7b284d20ee1119
bels <bels11@yahoo.com>
2010-09-18 14:22:56 -0400

    - changed mail_to_ticket to use Mail::IMAPClient instead.  It seems to be more documented - added the password for the mail user - added the ticket details div to the body for ticket lookup

--- mail_to_ticket.pl
+++ mail_to_ticket.pl
@@ -3,10 +3,13 @@ use warnings;
 
 use lib './libs';
 use ReadConfig;
-use IMAP::Client;
+use Mail::IMAPClient;
 
 my $config = ReadConfig->new(config_type =>'YAML',config_file => "config.yml");
 
 $config->read_config;
 
-my $imap = new IMAP::Client($config->{'mail_server'};
+my $imap = Mail::IMAPClient->new(Server => $config->{'mail_server'},User => $config->{'ticket_mail_account'},Password => $config->{'password'},Port => 443, SSL => 0);
+
+$imap->starttls() or die "Starttls failed in mail_to_ticket.pl";
+
