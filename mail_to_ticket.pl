1237891bc86f3f7344f305db14d7b416d0963bf8
bels <bels11@yahoo.com>
2010-09-20 12:43:40 -0400

    - added more error checking in mail_to_ticket

--- mail_to_ticket.pl
+++ mail_to_ticket.pl
@@ -11,7 +11,7 @@ my $config = ReadConfig->new(config_type =>'YAML',config_file => "config.yml");
 
 $config->read_config;
 
-my $imap = Mail::IMAPClient->new(Server => $config->{'mail_server'},User => $config->{'ticket_mail_account'},Password => $config->{'ticket_mail_password'},Port => 993, SSL => 1) or die "IMAP Failure: $@";
+my $imap = Mail::IMAPClient->new(Server => $config->{'mail_server'},User => $config->{'ticket_mail_account'},Password => $config->{'ticket_mail_password'},Port => 993, SSL => 1) or die "IMAP Failure: $!";
 
 #$imap->starttls() or die "Starttls failed in mail_to_ticket.pl"; #not needed right now but this does need to be an option in case the mail server only uses this
 
@@ -24,7 +24,7 @@ my $email_file = "tickets.mail";
 open TICKETS, ">>$email_file" or die "Was not able to append to $email_file";
 
 foreach my $msg (@msgs) {
-	$imap->message_to_file(<TICKETS>, $msg);
+	$imap->message_to_file(<TICKETS>, $msg) or die "Error writing email to file: $!";
 	$imap->delete_message($msg) or die "Couldn't delete message from server"; #Sets the Deleted flag
 }
 
