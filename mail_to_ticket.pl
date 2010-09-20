3011e59d33c005c703c9b1fd322096d70c58dc0f
bels <bels11@yahoo.com>
2010-09-20 12:35:58 -0400

    - Changed how we connect to the IMAP server

--- mail_to_ticket.pl
+++ mail_to_ticket.pl
@@ -11,9 +11,9 @@ my $config = ReadConfig->new(config_type =>'YAML',config_file => "config.yml");
 
 $config->read_config;
 
-my $imap = Mail::IMAPClient->new(Server => $config->{'mail_server'},User => $config->{'ticket_mail_account'},Password => $config->{'ticket_mail_password'},Port => 443, SSL => 0) or die "IMAP Failure: $@";
+my $imap = Mail::IMAPClient->new(Server => $config->{'mail_server'},User => $config->{'ticket_mail_account'},Password => $config->{'ticket_mail_password'},Port => 993, SSL => 1) or die "IMAP Failure: $@";
 
-$imap->starttls() or die "Starttls failed in mail_to_ticket.pl";
+#$imap->starttls() or die "Starttls failed in mail_to_ticket.pl"; #not needed right now but this does need to be an option in case the mail server only uses this
 
 $imap->select("INBOX") or die "Select 'Inbox' error: ", $imap->LastError, "\n";
 
