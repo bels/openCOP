621b8e4d2f38dc82b683b03bda33da9755802ce3
bels <bels11@yahoo.com>
2010-09-20 10:57:07 -0400

    - There is now a theoritical working mail fetcher for helpdesk system.  It needs testing still and is very primitive

--- mail_to_ticket.pl
+++ mail_to_ticket.pl
@@ -1,3 +1,5 @@
+# This script will download and remove all of the messages from the current mail server  for later processing
+
 use strict;
 use warnings;
 
@@ -13,3 +15,23 @@ my $imap = Mail::IMAPClient->new(Server => $config->{'mail_server'},User => $con
 
 $imap->starttls() or die "Starttls failed in mail_to_ticket.pl";
 
+$imap->select("INBOX") or die "Select 'Inbox' error: ", $imap->LastError, "\n";
+
+my @msgs = $imap->search('ALL') or die "Couldn't get all messages\n";
+
+my $email_file = "tickets.mail";
+
+open TICKETS, ">>$email_file" or die "Was not able to append to $email_file";
+
+foreach my $msg (@msgs) {
+	$imap->message_to_file(TICKETS, $msg);
+	$imap->delete_message($msg) or die "Couldn't delete message from server"; #Sets the Deleted flag
+}
+
+close(TICKETS);
+
+$imap->expunge; #this removes messages that have the Deleted flag set
+
+$imap->close("INBOX");
+
+$imap->logout();
