ee04f4e643b0102f68e02fe9219ccdbde11b990d
bels <bels11@yahoo.com>
2010-09-28 18:36:59 -0400

    - Added a little styling to the ticket details.  Nothing major

--- ticket_details.pl
+++ ticket_details.pl
@@ -32,12 +32,18 @@ if($authenticated == 1)
 	my %ticket_statuses = (1 => "New",2 => "In Progress",3 => "Waiting Customer",4 => "Waiting Vendor",5 => "Waiting Other",5 => "Closed", 6 => "Completed"); 
 	my %priorities = (0 => "Low",1 =>"Normal",2 => "High",3=>"Business Critical");
 
-	print  qq(<form action="update_ticket.pl" method="POST"><input type="hidden" name="ticket_number" value="$results->{'ticket'}"><label for="priority">Priority:</label><span id="priority" name="priority">$priorities{$results->{'priority'}}</span><br/>
-		<label for="ticket_number">Ticket Number:</label><span id="ticket_number" name="ticket_number">$results->{'ticket'}</span><label for="author">Author:</label><span id="author" name="author">$results->{'author'}</span><label for="contact">Contact:</label><input type="text" id="contact" name="contact" value="$results->{'contact'}"><label for="contact_phone">Contact Phone:</label><input type="text" id="contact_phone" name="contact_phone" value="$results->{'contact_phone'}"><label for="contact_email">Contact Email:</label><input type="text" id="contact_email" name="contact_email" value="$results->{'contact_email'}"><br/>
-		<label for="status">Ticket Status:<span id="status" name="status">$ticket_statuses{$results->{'status'}}</span><label for="site">Site:</label><input type="text" id="site" name="site" value="$results->{'site'}"><label for="location">Location:</label><input type="text" id="location" name="location" value="$results->{'location'}"><label for="requested_on">Requested On:</label><span id="requeseted_on" name="requested_on">$results->{'requested'}</span><label for="last_updated">Last Updated:</label><span id="last_updated" name="last_updated">$results->{'updated'}</span><br/>
+	print  qq(<form action="update_ticket.pl" method="POST" id="update_form"><input type="hidden" name="ticket_number" value="$results->{'ticket'}"><label for="priority">Priority:</label><span id="priority" name="priority">$priorities{$results->{'priority'}}</span><br/>
+		<label for="ticket_number">Ticket Number:</label><span id="ticket_number" name="ticket_number">$results->{'ticket'}</span><label for="author">Author:</label><span id="author" name="author">$results->{'author'}</span><br/>
+		<label for="contact">Contact:</label><input type="text" id="contact" name="contact" value="$results->{'contact'}"><label for="contact_phone">Contact Phone:</label><input type="text" id="contact_phone" name="contact_phone" value="$results->{'contact_phone'}"><label for="contact_email">Contact Email:</label><input type="text" id="contact_email" name="contact_email" value="$results->{'contact_email'}"><br/>
+		<label for="status">Ticket Status:</label><span id="status" name="status">$ticket_statuses{$results->{'status'}}</span><label for="site">Site:</label><input type="text" id="site" name="site" value="$results->{'site'}"><label for="location">Location:</label><input type="text" id="location" name="location" value="$results->{'location'}"><br/>
+		<label for="requested_on">Requested On:</label><span id="requeseted_on" name="requested_on">);
+	print substr($results->{'requested'},0,19);
+	print qq(</span><label for="last_updated">Last Updated:</label><span id="last_updated" name="last_updated">);
+	print substr($results->{'updated'},0,19);
+	print qq(</span><br/>
 		<label for="problem">Problem:</label><span id="problem" name="problem">$results->{'problem'}</span><br/>
-		<label for="troubleshoot">Troubleshooting Tried:</label><textarea cols="80" rows="10" id="troubleshooting" name="troubleshooting">$results->{'troubleshot'}</textarea><br/>
-		<label for="notes">Notes:</label><textarea rows="10" cols="80" id="notes" name="notes">$results->{'notes'}</textarea><br/>
+		<label for="troubleshoot">Troubleshooting Tried:</label><textarea cols="80" rows="8" id="troubleshooting" name="troubleshooting">$results->{'troubleshot'}</textarea><br/>
+		<label for="notes">Notes:</label><textarea rows="8" cols="80" id="notes" name="notes">$results->{'notes'}</textarea><br/>
 		<input type="submit" value="Update">
 	);
 }	
