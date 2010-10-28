153858d1fca4c635b9b0b782e4779d688c5a246b
bels <bels11@yahoo.com>
2010-10-28 09:51:08 -0400

    - Added functionality to allow technicians to submit tickets

--- process_mail.pl
+++ process_mail.pl
@@ -64,6 +64,12 @@ for my $line (@mail_data)
 		my $sth = $dbh->prepare($query);
 		$sth->execute;
 		my $alias = $sth->fetchrow_hashref;
+		if(!defined($alias)) #this allows technicians to submit tickets through email. if the email can't be found in the customers table it then searches the users(technicians)
+		{
+			$query = "select alias from users where email = '$sender'";
+			$sth->execute;
+			$alias = $sth->fetchrow_hashref;
+		}
 		my $data = {site => "",barcode => "",location =>"",author => $sender,contact => $sender,phone => "",troubelshoot=> "",section=>"",problem=>$body,priority =>"Normal",serial=>"",email=>$sender,tech => "", notes => "", submitter  => $alias->{'alias'}}; #This part will need to be improved.  Right now I am leaving a lot of fields blank that with some investigation could be filled out.  For example, I won't know what site someone is sending the ticket in from
 								#unless I lookup in the database for a matching email address and then checking what site that person is at.  Also, the persons name could be looked up by email address.  This is something that isn't feasible now but should be in the future
 		$ticket->submit(db_type => $config->{'db_type'},db_name=> $config->{'db_name'},user =>$config->{'db_user'},password => $config->{'db_password'},data => $data);
