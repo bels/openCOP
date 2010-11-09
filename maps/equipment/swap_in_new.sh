ad4a6eff1bd7b437fca4da2f3ae95c192bc96e3f
Eric Buehler <ebuehler@lfmcorp.com>
2010-11-09 16:11:01 -0500

    - Removed 90% of the unused files from Collin's version.

--- maps/equipment/swap_in_new.sh
+++
@@ -1,6 +0,0 @@
-mv create.form     create.form_old
-mv edit.form       edit.form_old
-
-mv create.form_new create.form
-mv edit.form_new   edit.form
-
--- templates/equipment/swap_in_new.sh
+++
@@ -1,5 +0,0 @@
-mv create.html     create.html_old
-mv edit.html       edit.html_old
-
-mv create.html_new create.html
-mv edit.html_new   edit.html
