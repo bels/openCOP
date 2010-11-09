9454b2b068c6a3186d711f92e3f39520d1d6e6c1
Eric Buehler <ebuehler@lfmcorp.com>
2010-11-09 16:15:41 -0500

    - Removed the last of what remains of Collin'g version, except what's in doc/.

--- datereport.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-use strict;
-use warnings;
-use lib "($ENV{'HOME'}/src/CCBOE/app/HTML--Template";
-
-use CCBOEHD::DateReport;
-my $cgiapp = new CCBOEHD::DateReport;
-$cgiapp->run;
-
--- form.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-use strict;
-use warnings;
-use lib "($ENV{'HOME'}/src/CCBOE/app/HTML--Template";
-
-use CCBOEHD::Form;
-my $cgiapp = new CCBOEHD::Form;
-$cgiapp->run;
-
