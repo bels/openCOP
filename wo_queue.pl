a215b706c410d84038dfed206ba9b96e3d0c7a13
bels <bels11@yahoo.com>
2010-12-03 15:13:01 -0500

    - More wo queue work

--- wo_queue.pl
+++ wo_queue.pl
@@ -6,7 +6,7 @@ use CGI;
 use ReadConfig;
 use Template;
 use SessionFunctions;
-use CustomerFunctions;
+use UserFunctions;
 
 my $config = ReadConfig->new(config_type =>'YAML',config_file => "config.yml");
 
@@ -27,6 +27,8 @@ if($authenticated == 1)
 {
 	my $dbh = DBI->connect("dbi:$config->{'db_type'}:dbname=$config->{'db_name'}",$config->{'db_user'},$config->{'db_password'})  or die "Database connection failed in $0";
 	my $query = "select * from wo where active";
+	my $user = UserFunctions->new(db_name=> $config->{'db_name'},user =>$config->{'db_user'},password => $config->{'db_password'},db_type => $config->{'db_type'});
+	my $id = $session->get_id_for_session(auth_table => $config->{'auth_table'},id => $cookie{'id'});
 	
 	my $sth = $dbh->prepare($query);
 	$sth->execute;
@@ -39,7 +41,7 @@ if($authenticated == 1)
 
 	my $file = "wo_queue.tt";
 	my $title = $config->{'company_name'} . " - Helpdesk Portal";
-	my $vars = {'title' => $title,'styles' => \@styles,'javascripts' => \@javascripts,'keywords' => $meta_keywords,'description' => $meta_description, 'company_name' => $config->{'company_name'}, logo => $config->{'logo_image'}, work_orders => $results};
+	my $vars = {'title' => $title,'styles' => \@styles,'javascripts' => \@javascripts,'keywords' => $meta_keywords,'description' => $meta_description, 'company_name' => $config->{'company_name'}, logo => $config->{'logo_image'}, work_orders => $results,is_admin => $user->is_admin(id => $id)};
 	
 	print "Content-type: text/html\n\n";
 
---
+++ wo_ticket_lookup.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/env perl
+
+use strict;
+use lib './libs';
+use CGI;
+use ReadConfig;
+use Template;
+use SessionFunctions;
+
+my $config = ReadConfig->new(config_type =>'YAML',config_file => "config.yml");
+
+$config->read_config;
+
+my $session = SessionFunctions->new(db_name=> $config->{'db_name'},user =>$config->{'db_user'},password => $config->{'db_password'},db_type => $config->{'db_type'});
+my $q = CGI->new();
+my %cookie = $q->cookie('session');
+
+my $authenticated = 0;
+
+if(%cookie)
+{
+	$authenticated = $session->is_logged_in(auth_table => $config->{'auth_table'},id => $cookie{'id'},session_key => $cookie{'session_key'});
+}
+
+if($authenticated == 1)
+{
+	my $dbh = DBI->connect("dbi:$config->{'db_type'}:dbname=$config->{'db_name'}",$config->{'db_user'},$config->{'db_password'})  or die "Database connection failed in $0";
+	my $query = "select * from wo_ticket where wo_id = $wo_number";
+
+}
+else
+{
+	print $q->redirect(-URL => $config->{'index_page'});
+}
