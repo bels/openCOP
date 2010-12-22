a03936fa35d1d3bc7b828b420826258f12617642
Eric Buehler <ebuehler@lfmcorp.com>
2010-12-22 15:10:46 -0500

    - Changed 'Change Password' on customer side to 'Change Settings.'. Made the passowrd change section of the page conditional.

--- javascripts/password.js
+++ javascripts/password.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
-	$("#password_form").validate();
-	$("#old_password").rules("add",{required: true});
-	$("#password1").rules("add",{required: true});
-	$("#password2").rules("add",{required: true});
-	$("#password2").rules("add",{equalTo: "#password1"});
+	if($("#password_form").length){
+		$("#password_form").validate();
+		$("#old_password").rules("add",{required: true});
+		$("#password1").rules("add",{required: true});
+		$("#password2").rules("add",{required: true});
+		$("#password2").rules("add",{equalTo: "#password1"});
+	}
 
 	$("#email_form").validate();
 	$("#email1").rules("add",{required: true, email: true});
