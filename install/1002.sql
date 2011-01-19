dd037b6a8b3cdc84eb854d68219cde5f1984ecd8
Eric Buehler <ebuehler@lfmcorp.com>
2011-01-19 14:45:45 -0500

    - More updates for 1.0.02

--- install/1002.sql
+++ install/1002.sql
@@ -1,4 +1,126 @@
---##
+DROP TYPE IF EXISTS ticket_holder CASCADE;
+CREATE TYPE ticket_holder AS (
+	ticket INTEGER,
+	priority VARCHAR(255),
+	priority_id INTEGER,
+	name VARCHAR(255),
+	section_id INTEGER,
+	status VARCHAR(255),
+	status_id VARCHAR(255),
+	problem TEXT,
+	contact VARCHAR(255),
+	location VARCHAR(255)
+);
+CREATE OR REPLACE FUNCTION lookup_ticket(
+	section_val INTEGER,
+	alias_id_val INTEGER
+) RETURNS SETOF ticket_holder AS $$
+DECLARE
+	r ticket_holder%rowtype;
+	read BOOLEAN;
+	complete BOOLEAN;
+BEGIN
+		SELECT INTO read
+				bool_or(section_aclgroup.aclread)
+			from
+				section_aclgroup
+				join
+					section on section.id = section_aclgroup.section_id
+				join
+					aclgroup on aclgroup.id = section_aclgroup.aclgroup_id
+			where
+				section_aclgroup.section_id = section_val
+			and (
+				section_aclgroup.aclgroup_id in (
+					select
+						aclgroup_id
+					from
+						alias_aclgroup
+					where
+						alias_id = alias_id_val
+				)
+			) and
+				not deleted
+		;
+
+		SELECT INTO complete
+				bool_or(section_aclgroup.aclcomplete)
+			from
+				section_aclgroup
+				join
+					section on section.id = section_aclgroup.section_id
+				join
+					aclgroup on aclgroup.id = section_aclgroup.aclgroup_id
+			where
+				section_aclgroup.section_id = section_val
+			and (
+				section_aclgroup.aclgroup_id in (
+					select
+						aclgroup_id
+					from
+						alias_aclgroup
+					where
+						alias_id = alias_id_val
+				)
+			) and
+				not deleted
+		;
+	IF complete THEN
+		FOR r IN
+			SELECT
+				f.ticket,
+				f.priority,
+				f.priority_id,
+				f.section AS name,
+				f.section_id,
+				f.status,
+				f.status_id,
+				f.problem,
+				f.contact,
+				f.location
+			FROM
+				friendly_helpdesk AS f
+			WHERE
+				f.status_id NOT IN ('7')
+			AND
+				f.active
+			AND
+				f.section_id = section_val
+		LOOP
+			RETURN NEXT r;
+		END LOOP;
+	
+	ELSIF read THEN
+		FOR r IN
+			SELECT
+				f.ticket,
+				f.priority,
+				f.priority_id,
+				f.section AS name,
+				f.section_id,
+				f.status,
+				f.status_id,
+				f.problem,
+				f.contact,
+				f.location
+			FROM
+				friendly_helpdesk AS f
+			WHERE
+				f.status_id NOT IN ('6','7')
+			AND
+				f.active
+			AND
+				f.section_id = section_val
+		LOOP
+			RETURN NEXT r;
+		END LOOP;
+	
+	ELSE
+		RETURN NEXT r;
+	END IF;
+	RETURN;
+END;
+$$ LANGUAGE plpgsql;
 CREATE OR REPLACE FUNCTION update_ticket(
 	ticket_number BIGINT,
 	site_text text,
@@ -79,4 +201,3 @@ BEGIN
 	RETURN ticket_number;
 END;
 $$ LANGUAGE plpgsql;
---$$
