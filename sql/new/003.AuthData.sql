5321daab1b210b875f809dc65e690965050abf29
Bels <cio-justin@infinity-ts.com>
2017-06-28 08:10:46 -0400

    - Changed data provided to match the new function signature

--- sql/new/003.AuthData.sql
+++ sql/new/003.AuthData.sql
@@ -4,4 +4,4 @@ INSERT INTO profile_data_type (description) VALUES ('mobile phone');
 INSERT INTO profile_data_type (description) VALUES ('home phone');
 INSERT INTO profile_data_type (description) VALUES ('work phone');
 
-perform select register('Admin','password','admin@example.com'); --TODO This needs to be change per install to the correct domain
+perform select register('Admin','User','password','admin@example.com'); --TODO This needs to be change per install to the correct domain
