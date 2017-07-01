8b0122f1f6f35a8c46c34ae2ca3fc33785fa2491
Bels <cio-justin@infinity-ts.com>
2017-07-01 10:55:20 -0400

    - Edit form looks rightish now - Troubleshooting does indeed save now

--- public/js/private/ticket.js
+++ public/js/private/ticket.js
@@ -3,13 +3,17 @@
 		$('.edit.btn').click(function(){
 			var $this = $(this);
 			$('.static').each(function(index,element){
-				console.log(element);
 				$(element).addClass('hidden');
 			});
 			$('.input').each(function(index,element){
 				$(element).removeClass('hidden');
 			});
 			$this.hide();
+			$('.save.btn').removeClass('hidden');
+		});
+		
+		$('.save.btn').click(function(){
+			
 		});
 	});
 })();
