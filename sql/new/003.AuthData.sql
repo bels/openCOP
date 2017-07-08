51dbb3fadf264e4dfdfaa20131f75e9a083bbc9a
Bels <bels11@yahoo.com>
2017-07-08 10:58:04 -0400

    - Fixed SQL syntax error

--- sql/new/003.AuthData.sql
+++ sql/new/003.AuthData.sql
@@ -5,5 +5,5 @@ INSERT INTO profile_data_type (description) VALUES ('home phone');
 INSERT INTO profile_data_type (description) VALUES ('work phone');
 INSERT INTO profile_data_type (description) VALUES ('account_type');
 
-perform select register('Admin','User','password','admin@example.com'); --TODO This needs to be change per install to the correct domain
-perform select update_profile((select id from users where login_identifier='admin@example.com'),'account_type','technician');
+select * from register('Admin','User','password','admin@example.com'); --TODO This needs to be change per install to the correct domain
+select * from update_profile((select id from users where login_identifier='admin@example.com'),'account_type','technician');
