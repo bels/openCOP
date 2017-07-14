fe799f2fd76e71680a95a8dc511b5618933f1472
User Bels <bels@shaggy.infinity-ts.com>
2017-07-14 14:59:32 -0400

    - Clicking the company name now redirects to the index page as someone would expect - The index page isn't just a static login anymore.  It will detect if you are logged in and if you are it will redirect

--- lib/Opencop/Controller/Core.pm
+++ lib/Opencop/Controller/Core.pm
@@ -5,6 +5,11 @@ use Mojo::Base 'Mojolicious::Controller';
 sub index {
 	my $self = shift;
 
+	my $session = $self->session->{'id'} // '00000000-0000-0000-0000-000000000000';
+	if($self->auth->verifySession($session)){
+		$self->redirect_to($self->flash('destination')) and return if defined $self->flash('destination');
+		$self->redirect_to($self->config->{'technician_landing_page'}) and return;
+	}
 }
 
 sub dashboard{
