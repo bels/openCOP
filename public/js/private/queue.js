141ae90a38c023e128c0b4d5bff4a96d45152df2
User Bels <bels@shaggy.infinity-ts.com>
2017-08-01 15:43:12 -0400

    - I think the default sorting is better - Column reordering

--- public/js/private/queue.js
+++ public/js/private/queue.js
@@ -1,7 +1,36 @@
 ;(function(){
 
 	$(function(){
-		$('.queue .table').DataTable({paging: false});
+		$.fn.dataTable.ext.type.order['status-sort'] = function ( d ) {
+		    switch ( d ) {
+		        case 'New':    return 1;
+		        case 'In Progress': return 2;
+		        case 'Waiting Customer':   return 3;
+		        case 'Waiting Vendor':   return 4;
+		        case 'Waiting Other':   return 5;
+		        case 'Closed':   return 6;
+		        case 'Completed':   return 7;
+		    }
+		    return 0;
+		};
+		$('.queue .table').DataTable({
+			'colReorder': true,
+			'paging': false,
+			'aoColumns': [
+				null,
+				null,
+				null,
+				null,
+				{'orderSequence': ['asc']},
+				null
+			],
+			'columnDefs': [
+				{
+					'type': 'status-sort',
+					'targets': 4
+				}
+			]
+		});
 		$('td').click(function(){
 			var $this = $(this);
 			window.location = '/ticket/' + $this.closest('tr').data('id');
